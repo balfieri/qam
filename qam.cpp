c71637c83369cf766d0c0abfa85d6642b78956bf
Robert A Alfieri <balfieri@nvidia.com>
2018-11-05 22:53:54 -0500

    bug fix; works well

--- qam.cpp
+++ qam.cpp
@@ -6,31 +6,38 @@
 #include <iostream>
 #include "stdlib.h"
 
+static constexpr bool     debug              = false;
 static constexpr uint32_t N_SQRT             = 4;
 static constexpr double   N_SQRT_F           = N_SQRT;
 static constexpr uint32_t N                  = N_SQRT * N_SQRT;
-static constexpr uint32_t INIT_PHASE_CNT_LG2 = 4;
+static constexpr uint32_t INIT_PHASE_CNT_LG2 = 8;
 static constexpr uint32_t INIT_PHASE_CNT     = 1 << INIT_PHASE_CNT_LG2;
 static constexpr double   PI_DIV_2           = M_PI / 2.0;
+static constexpr double   EPSILON            = 1e-10;
 
 int main( int argc, const char * argv[] )
 {
     double init_phase[N_SQRT]; 
     init_phase[0] = 0.0;  // can always fix first level
-    const uint32_t init_phase_total_cnt = INIT_PHASE_CNT * (1 << N_SQRT);
+    uint32_t init_phase_total_cnt = 1;
+    for( uint32_t i = 1; i < N_SQRT; i++ )
+    {
+        init_phase_total_cnt *= INIT_PHASE_CNT;
+    }
+
     double best_min_dist = 0.0;
     double x_best[N];
     double y_best[N];
     double x[N];
     double y[N];
-    std::cout << "init_phase_total_cnt=" << init_phase_total_cnt << "\n";
+    if ( debug ) std::cout << "init_phase_total_cnt=" << init_phase_total_cnt << "\n";
     for( uint32_t i = 0; i < init_phase_total_cnt; i++ )
     {
         for( uint32_t j = 1; j < N_SQRT; j++ )
         {
             uint32_t nom = (i >> ((j-1)*INIT_PHASE_CNT_LG2)) & (INIT_PHASE_CNT - 1);
-            init_phase[j] = M_PI/4.0 * double(nom)/double(INIT_PHASE_CNT);
-            std::cout << "i=" << i << " init_phase[" << j << "]=" << init_phase[j] << " (nom=" << nom << ")\n";
+            init_phase[j] = PI_DIV_2 * double(nom)/double(INIT_PHASE_CNT);
+            if ( debug ) std::cout << "i=" << i << " init_phase[" << j << "]=" << init_phase[j] << " (nom=" << nom << ")\n";
         }
 
         // choose point locations
@@ -45,14 +52,16 @@ int main( int argc, const char * argv[] )
             {
                 x[k] = a * std::cos( p );
                 y[k] = a * std::sin( p );
-                std::cout << "    xy[" << a << "," << p << "]=[" << x[k] << "," << y[k] << "]\n";
+                if ( x[k] >= -EPSILON && x[k] <= EPSILON ) x[k] = 0.0;
+                if ( y[k] >= -EPSILON && y[k] <= EPSILON ) y[k] = 0.0;
+                if ( debug ) std::cout << "    xy[" << a << "," << p << "]=[" << x[k] << "," << y[k] << "]\n";
                 if ( k != 0 ) {
                     for( uint32_t kk = 0; this_min_dist > best_min_dist && kk < k; kk++ )
                     {
                         double x_diff = x[k] - x[kk];
                         double y_diff = y[k] - y[kk];
                         double dist = std::sqrt( x_diff*x_diff + y_diff*y_diff );
-                        std::cout << "        dist=" << dist << " xy_diff=[" << x_diff << "," << y_diff << "]\n";
+                        if ( debug ) std::cout << "        dist=" << dist << " xy_diff=[" << x_diff << "," << y_diff << "]\n";
                         if ( dist == 0 ) exit( 1 );
                         if ( dist < this_min_dist ) this_min_dist = dist;
                     }
@@ -71,7 +80,7 @@ int main( int argc, const char * argv[] )
         }
     }
 
-    std::cout << "Best minimum distance: " << best_min_dist << "\n";
+    std::cout << "\nBest minimum distance: " << best_min_dist << "\n";
     std::cout << "Points: xy=\n";
     for( uint32_t k = 0; k < N; k++ )
     {
